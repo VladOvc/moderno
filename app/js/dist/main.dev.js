8f011efc7147a7522ce4c871eba8dbfbcc99b546
VladOvc <ovchar2000@gmail.com>
2020-08-28 23:34:42 +0300

    added new-item page, all icomoon fonts

--- app/js/dist/main.dev.js
+++ app/js/dist/main.dev.js
@@ -1,19 +1,17 @@
-$(function(){
+"use strict";
 
-
+$(function () {
   $(".rate-star").rateYo({
     rating: 5,
     readOnly: true,
-    starWidth: "12px",
+    starWidth: "12px"
   });
-
   $('.product-slider__inner').slick({
     dots: true,
     arrows: false,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 4
   });
-
   $(".js-range-slider").ionRangeSlider({
     type: "double",
     min: 0,
@@ -24,19 +22,15 @@ $(function(){
     hide_min_max: true,
     prefix: "$"
   });
-
-  $('.icon-th-list').on('click', function(){
+  $('.icon-th-list').on('click', function () {
     $('.product-page__items .product__item').addClass('list');
     $('.icon-th-list').addClass('active');
     $('.icon-th-large').removeClass('active');
   });
-
-  $('.icon-th-large').on('click', function(){
+  $('.icon-th-large').on('click', function () {
     $('.product-page__items .product__item').removeClass('list');
     $('.icon-th-list').removeClass('active');
     $('.icon-th-large').addClass('active');
   });
-
   var mixer = mixitup('.products__inner-box');
-
 });
