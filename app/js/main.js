140f3cd1f722172850ea66e9344a69d595a8eaf3
VladOvc <ovchar2000@gmail.com>
2020-10-30 21:53:32 +0200

    added 404, contents, about us

--- app/js/main.js
+++ app/js/main.js
@@ -37,6 +37,23 @@ $(function(){
     $('.icon-th-large').addClass('active');
   });
 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-user').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+
+  $('.product-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
+
   var mixer = mixitup('.products__inner-box');
 
 });
